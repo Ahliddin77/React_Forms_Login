593729b3fa6a56d51d90edc1896e319835a1f682
Akhliddin Akhmadjonov <ahliddinahmadjonov1123@gmail.com>
2024-04-24 20:28:42 +0200

    refactoring multiple inputs to an object

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,21 @@
 import { useState } from "react";
 
 export default function Login() {
-  const [enteredEmail, setEnteredEmail] = useState("");
-  const [enteredPassword, setEnteredPassword] = useState("");
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(enteredEmail, enteredPassword);
+    console.log(enteredValues);
   }
 
-  function handleEmailChange(event) {
-    setEnteredEmail(event.target.value);
-  }
-
-  function handlePasswordChange(event) {
-    setEnteredPassword(event.target.value);
+  function handleInputChange(identifier, value) {
+    setEnteredValues((prev) => ({
+      ...prev,
+      [identifier]: value,
+    }));
   }
 
   return (
@@ -28,8 +29,10 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
-            onChange={handleEmailChange}
-            value={enteredEmail}
+            onChange={(event) => {
+              handleInputChange("email", event.target.value);
+            }}
+            value={enteredValues.email}
           />
         </div>
 
@@ -39,7 +42,10 @@ export default function Login() {
             id="password"
             type="password"
             name="password"
-            onChange={handlePasswordChange}
+            onChange={(event) => {
+              handleInputChange("password", event.target.value);
+            }}
+            value={enteredValues.password}
           />
         </div>
       </div>
